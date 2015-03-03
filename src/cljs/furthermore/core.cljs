3b5aaf5296ad52149dbc053d7e6828a0e8a680ae
Akiva <akiva@neko.local>
2015-03-02 19:08:33 -0600

    Sets up secretary routing with React-based page updates.

--- src/cljs/furthermore/core.cljs
+++ src/cljs/furthermore/core.cljs
@@ -9,24 +9,32 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:topics [] :posts []}))
+(defonce app-state (atom {:topics [] :posts [] :page :home}))
 
 (def application {:target (. js/document (getElementById "page-content"))})
 
 (defroute "/" []
-  (om/root
-   home/construct-page
-   app-state
-   application))
+  (let [cursor (om/ref-cursor (om/root-cursor app-state))]
+    (om/update! cursor :page :home)))
 
 (defroute "/contents" []
-  (om/root
-   topics/load-content
-   app-state
-   application))
+  (let [cursor (om/ref-cursor (om/root-cursor app-state))]
+    (om/update! cursor :page :contents)))
 
 (comment
   (defroute "/post/:id" [id]
     (js/console.log (str "Display post id " id))))
+
+(om/root
+ (fn [app owner]
+   (reify
+     om/IRender
+     (render [_]
+       (let [target-page (condp = (:page app)
+                               :home home/construct-page
+                               :contents topics/construct-page)]
+         (om/build target-page app)))))
+ app-state
+ application)
 
 (secretary/dispatch! window.location.pathname)
