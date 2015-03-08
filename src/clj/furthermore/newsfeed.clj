20378443034481812b3b01dc98920725ce56a152
Akiva <akiva@neko.local>
2015-03-08 12:54:03 -0500

    Adds ability to get RSS from API call.

--- src/clj/furthermore/newsfeed.clj
+++ src/clj/furthermore/newsfeed.clj
@@ -1,5 +1,7 @@
 (ns furthermore.newsfeed
   (:require [clj-rss.core :as rss]
+            [markdown.core :refer [md-to-html-string]]
+            [typographer.core :as t]
             [furthermore.logging :refer :all]
             [furthermore.models.posts :refer :all]
             [furthermore.utils :as utils]))
@@ -11,14 +13,16 @@
    :language "en-us"
    :pubDate (java.util.Date.)})
 
-(defn convert-to-rss-item
+(defn- convert-to-rss-item
   [entry]
   (let [post (get-post {:_id (:ref entry)})
         url (str utils/site-url (utils/create-url-path entry) (:url entry))
-        item {:title (:title entry)
+        item {:title (t/smarten (:title entry))
               :link url
               :guid url
-              :description (str "<![CDATA[ " (:body post) " ]]")
+              :description (-> (str "<![CDATA[ " (:body post) " ]]>")
+                               t/smarten
+                               md-to-html-string)
               :author (:authors post)
               :pubDate (utils/convert-to-java-date (:date entry))}
         item (if (seq? (:tags post))
@@ -26,11 +30,11 @@
                item)]
     item))
 
-(defn generate-feed
+(defn- generate-feed
   [items]
   (rss/channel-xml site-channel items))
 
-(defn publish-feed
+(defn get-feed
   []
   (->> (get-weblog)
        (filter #(not= :topic (:type %)))
