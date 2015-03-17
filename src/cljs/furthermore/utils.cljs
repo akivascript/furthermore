1e22e2a9d8c82979ce2dd3f92812b75dcb047f2b
Akiva <akiva@neko.local>
2015-03-16 19:57:22 -0500

    Removes some cruft.

--- src/cljs/furthermore/utils.cljs
+++ src/cljs/furthermore/utils.cljs
@@ -7,18 +7,3 @@
   (let [ts (-> timestamp js/Date. goog.date.DateTime.)]
     {:date (timef/unparse (timef/formatter "MMMM d, yyyy") ts)
      :time (timef/unparse (timef/formatter "hh:mm a") ts)}))
-
-(defn change-page
-  [dest]
-  (secretary/dispatch! dest))
-
-(defn set-url
-  [dest]
-  (let [url window.location.pathname]
-    (.pushState js/history url dest dest)))
-
-(defn navigate-to
-  [dest]
-  (do
-    (set-url dest)
-    (change-page dest)))
