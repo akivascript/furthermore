0b910c1b60abb6a0b519a82617e0c46d0cab1684
Akiva <akiva@neko.local>
2015-03-30 17:42:40 -0500

    Updates REPL dev environment.

--- env/dev/clj/furthermore/dev.clj
+++ env/dev/clj/furthermore/dev.clj
@@ -1,10 +1,26 @@
 (ns furthermore.dev
-  (:require [environ.core :refer [env]]
-            [leiningen.core.main :as lein]))
+  (:require [clojure.string :as str]
+            [environ.core :refer [env]]
+            [furthermore.logging :refer :all]
+            [furthermore.models.log :refer :all]
+            [furthermore.models.posts :refer :all]
+            [furthermore.models.static-pages :refer :all]
+            [furthermore.models.topics :refer :all]
+            [furthermore.newsfeed :refer :all]
+            [furthermore.pages :refer :all]
+            [furthermore.repository :refer :all]
+            [furthermore.server :refer :all]
+            [furthermore.twitter :refer :all]
+            [furthermore.utils :refer :all]))
 
-(def is-dev? (env :is-dev))
+(def is-dev? (env :dev))
 
-(defn start-figwheel []
-  (future
-    (print "Starting figwheel.\n")
-    (lein/-main ["figwheel"])))
+(defn rl-log [] (require '[furthermore.models.log :refer :all] :reload))
+(defn rl-pages [] (require '[furthermore.pages :refer :all] :reload))
+(defn rl-posts [] (require '[furthermore.models.posts :refer :all] :reload))
+(defn rl-repo [] (require '[furthermore.repository :refer :all] :reload))
+(defn rl-server [] (require '[furthermore.server :refer :all] :reload))
+(defn rl-static [] (require '[furthermore.models.static :refer :all] :reload))
+(defn rl-topics [] (require '[furthermore.models.topics :refer :all] :reload))
+(defn rl-twitter [] (require '[furthermore.twitter :refer :all] :reload))
+(defn rl-utils [] (require '[furthermore.utils :refer :all] :reload))
