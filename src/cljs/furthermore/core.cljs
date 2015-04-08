6d3424fc196b6174fe2ddd1fd4a6dfbd76becc7a
Akiva <akiva@neko.local>
2015-04-08 12:49:58 -0500

    Updates how the site data is initially loaded.

--- src/cljs/furthermore/core.cljs
+++ src/cljs/furthermore/core.cljs
@@ -1,5 +1,6 @@
 (ns furthermore.core
-  (:require [goog.events :as events]
+  (:require [ajax.core :as ajax]
+            [goog.events :as events]
             [goog.history.EventType :as EventType]
             [om.core :as om :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
@@ -7,12 +8,11 @@
             [secretary.core :as secretary :refer-macros [defroute]]
 
             [furthermore.home :refer [home-path home-view]]
+            [furthermore.topics :refer [contents-path]]
             [furthermore.posts :refer [post-path]]
             [furthermore.routing :refer [consume-events pub-chan sub-chan]]
             [furthermore.static-page :refer [static-path]]
-            [furthermore.topics :refer [contents-path]]
-            [furthermore.weblog :refer [updates-path]]
-            [furthermore.update :refer [update-path]])
+            [furthermore.weblog :refer [updates-path]])
   (:import goog.History))
 
 ;;
@@ -20,14 +20,13 @@
 ;;
 (enable-console-print!)
 
-(secretary/set-config! :prefix "#")
-
 (defonce app-state (atom {:topics []
-                          :post {:id nil
-                                 :post nil
-                                 :topic nil}
-                          :page nil
-                          :weblog []}))
+                          :posts []
+                          :updates []
+                          :pages {}}))
+
+
+(secretary/set-config! :prefix "#")
 
 (def application {:target (. js/document (getElementById "page-content"))})
 (def nav-bar {:target (. js/document (getElementById "nav-bar"))})
@@ -43,7 +42,7 @@
 ;; Layout
 ;;
 (defn nav-view
-  [data owner]
+  [app owner]
   (om/component
    (d/div {:class "container"}
           (d/div {:class "navbar-header"}
@@ -66,35 +65,48 @@
                        (d/li
                         (d/a {:href (static-path {:url "about"})} "About")))))))
 
-(om/root
- (fn [data owner]
-   (reify
-     om/IRender
-     (render [_]
-       (om/build nav-view data))))
- app-state
- nav-bar)
+(defn start-site
+  []
+  (letfn [(get-data [path url]
+        (let [root (om/root-cursor app-state)]
+            (ajax/GET url
+                        {:handler #(om/update! root path %)
+                        :error-handler #(.error js/console %)})))]
+    (get-data :posts "/api/posts")
+    (get-data :topics "/api/topics")
+    (get-data :updates "/api/weblog"))
 
-(om/root
- (fn [data owner]
-   (reify
-     om/IInitState
-     (init-state [_]
-       {:view home-view})
-     om/IDidMount
-     (did-mount [_]
-       (consume-events owner :change-view
-                       (fn [{:keys [view view-init-state view-name data]}]
-                         (om/set-state! owner :view view)
-                         (om/set-state! owner :view-init-state view-init-state)
-                         (om/set-state! owner :react-key view-name)
-                         (om/set-state! owner :opts data))))
-     om/IRenderState
-     (render-state [_ {:keys [view view-init-state react-key opts]}]
-       (om/build view data {:init-state view-init-state
-                            :react-key react-key
-                            :opts opts}))))
- app-state
- (assoc application :shared
-        {:sub-chan sub-chan
-         :pub-chan pub-chan}))
+  (om/root
+   (fn [app owner]
+     (reify
+       om/IRender
+       (render [_]
+         (om/build nav-view app))))
+   app-state
+   nav-bar)
+
+  (om/root
+   (fn [app owner]
+     (reify
+       om/IInitState
+       (init-state [_]
+         {:view home-view})
+       om/IDidMount
+       (did-mount [_]
+         (consume-events owner :change-view
+                         (fn [{:keys [view view-init-state view-name data]}]
+                           (om/set-state! owner :view view)
+                           (om/set-state! owner :view-init-state view-init-state)
+                           (om/set-state! owner :react-key view-name)
+                           (om/set-state! owner :opts data))))
+       om/IRenderState
+       (render-state [_ {:keys [view view-init-state react-key opts]}]
+         (om/build view app {:init-state view-init-state
+                             :react-key react-key
+                             :opts opts}))))
+   app-state
+   (assoc application :shared
+          {:sub-chan sub-chan
+           :pub-chan pub-chan})))
+
+(start-site)
