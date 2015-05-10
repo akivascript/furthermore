71689b444d17d571ba36eaef6707fe58abb82dd3
Akiva <akiva@neko.local>
2015-05-10 13:32:18 -0500

    Adds single post page.

--- src/clj/furthermore/server.clj
+++ src/clj/furthermore/server.clj
@@ -1,7 +1,7 @@
 (ns furthermore.server
   (:require [clojure.java.io :as io]
 
-            [compojure.core :refer [ANY context defroutes]]
+            [compojure.core :refer [ANY GET context defroutes]]
             [compojure.handler :refer [site]]
             [compojure.response :refer [render]]
             [compojure.route :refer [resources]]
@@ -10,48 +10,18 @@
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.params :refer [wrap-params]]
 
-            [furthermore.logging :refer [get-weblog]]
-            [furthermore.entities :refer [add-post
-                                          get-post
-                                          get-posts
-                                          get-post-references
-                                          get-topic
-                                          get-topics
-                                          get-topic-references]]
-            [furthermore.static-pages :refer [get-static-page]]
+            [furthermore.home :refer [display-home-page]]
+            [furthermore.post :refer [display-post-page]]
             ;[furthermore.newsfeed :refer [get-feed]]
             [furthermore.repository :refer [initialize-db-connection]])
   (:gen-class))
 
-(defresource update-site
-  [type]
-  :allowed-methods [:post]
-  :available-media-types ["application/edn"]
-  :post! (fn [ctx]
-           (add-post (slurp (get-in ctx [:request :body])) (keyword type))))
-
-(defresource return-result
-  [task]
-  :allowed-methods [:get]
-  :available-media-types ["application/edn"]
-  :handle-ok (pr-str task))
-
 (defroutes routes
   ;; API calls
-  (context "/api" []
-           (ANY "/page/:url" [url] (return-result (get-static-page {:url url})))
-           (ANY "/post/id/:id" [id] (return-result (get-post {:_id id})))
-           (ANY "/post/:id/refs" [id] (return-result (get-post-references id)))
-           (ANY "/post/url/:url" [url] (return-result (get-post {:url url})))
-           (ANY "/posts" [] (return-result (get-posts)))
-           (ANY "/topic/:id" [id] (return-result (get-topic {:_id id})))
-           (ANY "/topic/:id/refs" [id] (return-result (get-topic-references id)))
-           (ANY "/topics" [] (return-result (get-topics)))
-           (ANY "/update/:type" [type] (update-site type))
-           (ANY "/weblog" [] (return-result (get-weblog))))
+  (GET "/" [] (display-home-page))
+  (GET "/post/:title" [title] (display-post-page title))
   ;; Disabled until RSS feed is fixed (ANY "/rss.xml" [] (get-feed))
   ;; UI Calls
-  (ANY "/" [uri] (render (io/resource "public/shell.html") uri))
   (resources "/"))
 
 (def app
