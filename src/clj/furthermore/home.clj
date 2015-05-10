2382cb0507e12fa618b8dcf6835a201b6a3fa656
Akiva <akiva@neko.local>
2015-05-10 14:40:03 -0500

    Adjusts layout just a SMIDGE.

--- src/clj/furthermore/home.clj
+++ src/clj/furthermore/home.clj
@@ -14,36 +14,38 @@
         {:keys [date time]} (format-timestamp (:created-on post))
         excerpt? (contains? post :excerpt)]
     (html
-     [:div {:class "post"}
-      [:div {:class "title"}
-       [:a {:href (str "/post/" (:url post))}
-        (smarten (:title post))]]
-      (when (contains? post :subtitle)
-        [:div {:class "subtitle"}
-         (smarten (:subtitle post))])
-      (comment
-        (when (:tags post)
-          [:div {:class "tags text-right"}
-           (display-tags (:tags post))]))
-      (if excerpt?
-        [:div {:class "body"} (md-to-html-string (:excerpt post))]
-        [:div {:class "body"} (md-to-html-string (:body post))])
-      [:div {:class "footer"}
-       [:div {:class "row"}
-        [:div {:class "col-xs-12 col-sm-6"}
-         (when excerpt?
-           [:div {:class "continue"}
-            [:a {:href (str "/post/" (:url post))} "Continue &raquo;"]])]
-        [:div {:class "col-xs-12 col-sm-6"}
-         [:div {:class "small text-right date"}
-          "Filed under "
-          [:span {:class "topic"} (smarten (:title topic))]
-          [:br]
-          (str date " @ " time)
-          [:br]
-          (when-let [url (get-in post [:twitter :url])]
-            [:a {:href url
-                 :target "_blank"} "Tweeted!"])]]]]])))
+     [:div {:class "row"}
+      [:div {:class "col-xs-12 col-sm-8 col-sm-offset-2"}
+       [:div {:class "post"}
+        [:div {:class "title"}
+         [:a {:href (str "/post/" (:url post))}
+          (smarten (:title post))]]
+        (when (contains? post :subtitle)
+          [:div {:class "subtitle"}
+           (smarten (:subtitle post))])
+        (comment
+          (when (:tags post)
+            [:div {:class "tags text-right"}
+             (display-tags (:tags post))]))
+        (if excerpt?
+          [:div {:class "body"} (md-to-html-string (:excerpt post))]
+          [:div {:class "body"} (md-to-html-string (:body post))])
+        [:div {:class "footer"}
+         [:div {:class "row"}
+          [:div {:class "col-xs-12 col-sm-6"}
+           (when excerpt?
+             [:div {:class "continue"}
+              [:a {:href (str "/post/" (:url post))} "Continue &raquo;"]])]
+          [:div {:class "col-xs-12 col-sm-6"}
+           [:div {:class "small text-right date"}
+            "Filed under "
+            [:span {:class "topic"} (smarten (:title topic))]
+            [:br]
+            (str date " @ " time)
+            [:br]
+            (when-let [url (get-in post [:twitter :url])]
+              [:a {:href url
+                   :target "_blank"} "Tweeted!"])]]]]]]])))
 
 (defn display-home-page
   []
@@ -51,6 +53,4 @@
    (html
     [:div {:id "index"
            :class "container"}
-     [:div {:class "row"}
-      [:div {:class "col-xs-12 col-sm-8 col-sm-offset-2"}
-       (map display-post (filter #(= :post (:type %)) (get-posts)))]]])))
+     (map display-post (filter #(= :post (:type %)) (get-posts)))])))
