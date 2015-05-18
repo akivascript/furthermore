51a3f2ef2688fb70dbcd38a810ebbdbc59860b72
Akiva <akiva@neko.local>
2015-05-18 09:23:24 -0500

    Adjusts width of individual posts.

--- src/clj/furthermore/post.clj
+++ src/clj/furthermore/post.clj
@@ -69,6 +69,6 @@
     [:div {:id "post"
            :class "container"}
      [:div {:class "row"}
-      [:div {:class "col-xs-12 col-sm-8 col-sm-offset-2"}
+      [:div {:class "col-xs-12 col-sm-10 col-sm-offset-1"}
        (display-post (get-post {:url url}))]]])))
 
