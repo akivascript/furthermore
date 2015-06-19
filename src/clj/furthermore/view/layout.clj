abc25432043fe7af826245a3b08bd813bdc87115
Akiva <akiva@neko.local>
2015-06-19 15:06:34 -0500

    Updates HTML layout with new version number.

--- src/clj/furthermore/view/layout.clj
+++ src/clj/furthermore/view/layout.clj
@@ -93,13 +93,15 @@
        [:div {:class "hidden-xs col-sm-6 text-right small"}
         "&copy; 2015 Whatever, Akiva"[:br]
         "Powered by " [:a {:href "https://github.com/akivaschoen/furthermore"} "Furthermore"]
-        [:span {:class "small"} " (v0.6.5&beta;)"]
+        [:span {:class "small"} " (v0.7.0&beta;)"]
         [:br]
         "Clojure and ClojureScript are a-okay!"]
        [:div {:class "visible-xs-block hidden-sm hidden-lg text-center small"
               :style "padding-top: 10px;"}
         "&copy; 2015 Whatever, Akiva"[:br]
-        "Powered by " [:a {:href "https://github.com/akivaschoen/furthermore"} "Furthermore"][:br]
+        "Powered by " [:a {:href "https://github.com/akivaschoen/furthermore"} "Furthermore"]
+        [:span {:class "small"} " (v0.7.0&beta;)"]
+        [:br]
         "Clojure and ClojureScript are a-okay!"]
        [:div {:class "visible-xs-block hidden-sm hidden-lg text-center small"}
         "RSS feed coming soon!"[:br]
