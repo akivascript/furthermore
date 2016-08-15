811380af03a910272f8e8cb6c16c8df7170e11b1
Akiva <akiva@akivamail.com>
2016-08-15 16:58:57 -0500

    Formatting change.

--- project.clj
+++ project.clj
@@ -67,7 +67,9 @@
   :hooks [leiningen.sassc]
 
   :clean-targets ^{:protect false}
-  [:target-path [:cljsbuild :builds :app :compiler :output-dir] [:cljsbuild :builds :app :compiler :output-to]]
+  [:target-path
+   [:cljsbuild :builds :app :compiler :output-dir]
+   [:cljsbuild :builds :app :compiler :output-to]]
 
   :figwheel
   {:http-server-root "public"
