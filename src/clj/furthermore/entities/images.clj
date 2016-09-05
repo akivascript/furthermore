18ccee0a0f20cb794a4f1b301a1f94e3b43d09d8
Akiva <akiva@akivamail.com>
2016-09-05 07:55:10 -0500

    Refine Image record and add image-source encoding.

--- src/clj/furthermore/entities/images.clj
+++ src/clj/furthermore/entities/images.clj
@@ -1,53 +1,66 @@
 (ns furthermore.entities.images
-  (:require [clj-time.local :as ltime]
+  (:require [clojure.java.io :as io]
+
+            [clj-time.local :as ltime]
             [monger.util :as mutil]
 
             [furthermore.db.core :as db]
             [furthermore.entities.references :as refs :refer [->refs]]
-            [furthermore.util :as util]))
+            [furthermore.util :as util])
+  (:import (java.io ByteArrayOutputStream)))
 
 (defrecord Image
     [_id alt-text authors created-on filename image-source
-     kind last-updated log? refs tags title tweet? url])
+     kind last-updated log? refs tags title tweet?])
 
 (defn- image
   [params]
-  (let [date (ltime/local-now)
-        {:keys [_id alt-text authors created-on filename image-source
-                kind last-updated log? refs tags title tweet? url]
+  (let [{:keys [_id alt-text authors created-on filename image-source
+                kind last-updated log? refs tags title tweet?]
          :or {_id (mutil/random-uuid)
-              alt-text "image"
-              created-on date
+              alt-text "Image"
+              created-on (ltime/local-now)
               log? false
               refs #{}
               tags #{}
               title "New Image"
-              tweet? false
-              url (util/entity-url title)}} params]
+              tweet? false}} params]
     (map->Image {:_id _id
                  :alt-text alt-text
                  :authors (->refs authors)
+                 :created-on created-on
                  :filename filename
                  :image-source image-source
                  :kind :image
+                 :last-updated last-updated
                  :log? log?
                  :refs (->refs refs)
                  :tags (->refs tags)
                  :title title
-                 :tweet? tweet?
-                 :url url})))
+                 :tweet? tweet?})))
 
 (defn image?
   "Returns true if img is an Image."
   [img]
   (instance? Image img))
 
+(defn- load
+  "Loads an image binary from filename into Image entity img."
+  [img filename]
+  (let [output (ByteArrayOutputStream.)
+        input (io/input-stream filename)]
+    (io/copy input output)
+    (-> img
+        (assoc :image-source (.toByteArray output))
+        (assoc :filename (util/filename filename)))))
+
 (defn create
   "Returns an image entity."
-  [img]
+  [img filename]
   (cond
-    (nil? img) nil
-    (map? img) (image img)))
+    (and (map? img)
+         (string? filename)) (load (image img) filename)
+    :else nil))
 
 (defn save
   "Commits an image entity to the datastore."
